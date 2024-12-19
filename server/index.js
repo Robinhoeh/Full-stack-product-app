cda74ac5e26081f3d38a18e84c8cfb9ec270930a
robinwatson <drumsrobin@gmail.com>
2024-12-19 15:01:19 -0500

    add dynamic port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import productRoute from "./routes/product.route.js";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //middleware
 // json is not accepted by express by default
